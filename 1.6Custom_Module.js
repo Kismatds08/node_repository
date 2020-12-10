1e3dd606fc8571706234cd73a8492607f2a8b91c
Kismat D Shrestha <32634442+Kismatds08@users.noreply.github.com>
2020-12-09 20:26:40 -0600

    Add files via upload

---
+++ 1.6Custom_Module.js
@@ -0,0 +1,14 @@
+exports.myDateTime = function () {
+    return Date()
+}
+
+var a = 10
+
+class ModuleClass {
+
+}
+
+var b = 11
+// Module can  be group of classes with functions and variables
+// Module can also be a function
+// Module can also be a variable as well
